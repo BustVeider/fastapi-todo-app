8e5dbfba003100a09e6ce0ed32911262a39b5995
Timur <bulletstormgadzhimuradov@yandex.ru>
2026-09-04 12:17:44 +0300

    Implement full OAuth2 JWT authentication and protect task routes

--- myfeature.py
+++ myfeature.py
@@ -1,18 +1,30 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, status
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
 from database import engine, Base, get_db
 from models import TaskModel, UserModel
-from security import get_password_hash, verify_password
+from security import get_password_hash, verify_password, create_access_token, get_user_id_from_token
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
 Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Todo-api with PostgreSQL")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 class TaskSchema(BaseModel):
     title: str
 class UserCreateSchema(BaseModel):
     username: str
     password: str
+def get_currect_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    user_id = get_user_id_from_token(token)
+    if user_id is None:
+        raise HTTPException(status_code=401, detail="Cloud not validate credentials")
+
+    user = db.query(UserModel).filter(UserModel.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not found")
+    return user
 
 
 @app.get("/tasks", tags=["Task"])
@@ -21,8 +33,8 @@ def get_tasks(db: Session = Depends(get_db)):
     return {"tasks": tasks}
 
 @app.post("/tasks", tags=["Task"])
-def add_task(task: TaskSchema, db:Session = Depends(get_db)):
-    new_task = TaskModel(title=task.title)
+def add_task(task: TaskSchema, db:Session = Depends(get_db), current_user: UserModel = Depends(get_currect_user)):
+    new_task = TaskModel(title=task.title, user_id = current_user.id)
     db.add(new_task)
     db.commit()
     db.refresh(new_task)
@@ -58,8 +70,10 @@ def register_user(user: UserCreateSchema, db: Session= Depends(get_db)):
     return {"message": "User registreted successfully", "username": new_user.username}
 
 @app.post("/login", tags = ["Auth"])
-def login_user(user: UserCreateSchema, db: Session= Depends(get_db)):
-    db_user = db.query(UserModel).filter(UserModel.username == user.username).first()
-    if db_user is None or not verify_password(user.password, db_user.hashed_password):
+def login_user(form_data: OAuth2PasswordRequestForm = Depends(), db: Session= Depends(get_db)):
+    db_user = db.query(UserModel).filter(UserModel.username == form_data.username).first()
+    if db_user is None or not verify_password(form_data.password, db_user.hashed_password):
         raise HTTPException(status_code=404, detail="Incorrect username or password")
-    return {"message": "Login successful", "username": user.username}
+
+    access_token = create_access_token(data={"sub": str(db_user.id)})
+    return {"access_token": access_token , "token_type": "bearer"}
--- security.py
+++ security.py
@@ -1,4 +1,11 @@
 import bcrypt
+import  jwt
+from datetime import  datetime, timedelta
+from jwt.exceptions import InvalidTokenError
+
+SECRET_KEY = "my_super_secret_key_12345"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 def get_password_hash(password: str):
     pwd_bytes = password.encode('utf-8')
@@ -11,3 +18,19 @@ def verify_password(plain_password: str, hashed_password: str) ->bool:
     hashed_bytes = hashed_password.encode('utf-8')
     return bcrypt.checkpw(plain_bytes, hashed_bytes)
 
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp" : expire})
+    encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encode_jwt
+
+def get_user_id_from_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            return None
+        return int(user_id)
+    except InvalidTokenError:
+        return None
